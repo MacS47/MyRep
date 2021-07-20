6d8feb771eea38d11fb8f3605b6b9f54fac3ea7f
MacS47 <52472361+MacS47@users.noreply.github.com>
2021-07-20 15:39:38 -0300

    Update pitagoras.py

--- pitagoras.py
+++ pitagoras.py
@@ -1,10 +1,20 @@
+# -----------------------------------------------------------
 # Essa função tem como objetivo encontrar os valores para os 
 # catetos e a hipotenusa de um triângulo retângulo 
 # utilizando o Teorema de Pitágoras h²= a² + b²
+# -----------------------------------------------------------
 
 import math
 
+# A função receberá três parâmetros:
+# * h - hipotenusa
+# * a - 1º cateto
+# * b - 2º Cateto
+
 def pitagoras(h, a, b):
+    
+    # Após receber os valores, será verificado qual deles está ausente
+    
     if h == 0 and a != 0 and b != 0:
         h = math.sqrt((a**2)+(b**2))
         result = h
